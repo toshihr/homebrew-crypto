16986917d7118b9b02f46ef7f90dfc364dee7c93
kerug <keru.work@gmail.com>
2014-09-08 00:57:16 +0900

    initial commit

---
+++ testu01.rb
@@ -0,0 +1,45 @@
+require "formula"
+
+# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Testu01 < Formula
+  homepage "http://simul.iro.umontreal.ca/testu01/tu01.html"
+  url "http://simul.iro.umontreal.ca/testu01/TestU01.zip"
+  sha1 "f41af4bc71841e16dacc195930e6c3a191c9ab99"
+  version "1.2.3"
+
+  # depends_on "cmake" => :build
+  #depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    ENV.append "CFLAGS", "-Wno-return-type"
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    #system "make", "install" # if this fails, try separate make/make install steps
+    system "make"
+    lib.install "lib/libtestu01.a"
+    lib.install "libmylib.a"
+    lib.install "libprobdist.a"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test TestU`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
