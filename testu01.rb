2d494e0b84a29634d450d1e9c9198a2b23b46d74
kerug <keru.work@gmail.com>
2014-09-09 23:24:36 +0900

    fix install

--- testu01.rb
+++ testu01.rb
@@ -23,11 +23,11 @@ class Testu01 < Formula
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
-    #system "make", "install" # if this fails, try separate make/make install steps
-    system "make"
-    lib.install "lib/libtestu01.a"
-    lib.install "libmylib.a"
-    lib.install "libprobdist.a"
+    system "make", "install" # if this fails, try separate make/make install steps
+    # system "make"
+    # lib.install "lib/libtestu01.a"
+    # lib.install "libmylib.a"
+    # lib.install "libprobdist.a"
   end
 
   test do
